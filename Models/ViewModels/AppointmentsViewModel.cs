d7df6febc883340f8064a67afa7924d1bfa2e1c8
bfank9@gmail.com <bfank9@gmail.com>
2021-03-17 22:29:09 -0600

    View Model

---
+++ Models/ViewModels/AppointmentsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectOne.Models.ViewModels
+{
+    public class AppointmentsViewModel
+    {
+        public IEnumerable<SignUp> SignUp { get; set; }
+        public IEnumerable<TimeSlots> TimeSlots { get; set; }
+    }
+}
